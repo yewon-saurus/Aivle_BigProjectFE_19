e86819bdb0cab87334108d6783ea0acd75251968
zunza <fksfks015@naver.com>
2023-12-28 13:51:54 +0900

    마이페이지 항목들 수정

--- src/pages/account/MyPage/Profile.jsx
+++ src/pages/account/MyPage/Profile.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import {Card, CardHeader, CardBody, Image, Textarea} from "@nextui-org/react";
 import Sidebar2 from '../../../components/SideBar2';
+
 
 function Profile() {
     const [userData, setUserData] = useState(null);
@@ -24,24 +25,27 @@ function Profile() {
   return (
     <div>
         <Sidebar2 />
-        <div style={{padding:'63px', marginLeft:'256px', marginBottom:'200px'}}>
-            <Card className="py-4" style={{ width: '300px', height: '370px' }}>
+        <div style={{padding:'63px', marginLeft:'256px', marginBottom:'200px', display: 'flex', justifyContent: 'center', position: 'relative', top: '145px'}}>
+            <Card className="py-4" style={{ width: '300px', height: '460px'}}>
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                    <p className="text-tiny uppercase font-bold">Level.</p>
-                    <small className="text-default-500">
-                        {userData !== null ? userData.profile.user_level : 'Loading...'}
-                    </small>
-                    <h4 className="font-bold text-large">
-                        {userData !== null ? userData.profile.introduction : 'Loading...'}
-                    </h4>
-                </CardHeader>
-                <CardBody className="overflow-visible py-2">
-                    <Image
+                <p className="text-tiny uppercase font-bold">Level. {userData !== null ? userData.profile.user_level : 'Loading...'}</p>
+                <br />
+                <Image
                     alt="Card background"
                     className="object-cover rounded-xl"
                     src={userData !== null ? userData.profile.image : 'Loading...'}
                     width={270}
                     />
+                </CardHeader>
+                <CardBody className="overflow-visible py-2">
+                    <Textarea
+                        isReadOnly
+                        variant="bordered"
+                        labelPlacement="outside"
+                        // defaultValue="NextUI is a React UI library that provides a set of accessible, reusable, and beautiful components."
+                        value={userData !== null ? userData.profile.introduction : 'Loading...'}
+                        className="max-w-xs"
+                        />
                 </CardBody>
             </Card>
         </div>
