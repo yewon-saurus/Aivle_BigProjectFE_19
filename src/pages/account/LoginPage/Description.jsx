6acdd2ef3f74eac4f3a76985d2fa21555214694c
yewon-saurus <ehdnjs0804@gmail.com>
2024-01-05 15:41:01 +0900

    [FEAT][UI/UX]로그인/회원가입(+회원약관동의) 디자인 수정, 반응형 작업
    - tailwind css의 color var 대신 우리 프로젝트의 primary color로 수정
    - 반응형 간단하게 조치

--- src/pages/account/LoginPage/Description.jsx
+++ src/pages/account/LoginPage/Description.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Description() {
   return (
-    <div className='bg-blue-500 bg-opacity-50' style={{ flexBasis: '50%'}}>
+    <div className='lg:block hidden bg-blue-500 bg-opacity-50 w-[50%]'>
         <h1 style={{ color:'white'}}>Left</h1>
     </div>
   )
