a5c70adf3fb19eb52fb38a0f9aadf2b746300ffd
yewon-saurus <ehdnjs0804@gmail.com>
2024-01-11 14:58:21 +0900

    [FEAT]user profile 미니 프로필 API URL 수정

--- src/pages/board/UserProfile.jsx
+++ src/pages/board/UserProfile.jsx
@@ -8,7 +8,7 @@ function UserProfile({ userId, token }) {
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const response = await axios.get(`http://127.0.0.1:8000//accounts/profile/${userId}/`, {
+        const response = await axios.get(process.env.REACT_APP_API_URL + `/accounts/profile/${userId}/`, {
           headers: {
             'Authorization': `Token ${token}`
           }
--- src/pages/board/UserProfile2.jsx
+++ src/pages/board/UserProfile2.jsx
@@ -10,7 +10,7 @@ function UserProfile2({ userId, token, profiles, setProfiles }) {
       if (profiles[userId]) {
         setProfile(profiles[userId]);
       } else {
-        axios.get(`http://127.0.0.1:8000//accounts/profile/${userId}/`, {
+        axios.get(process.env.REACT_APP_API_URL + `/accounts/profile/${userId}/`, {
             headers: {
                 'Authorization': `Token ${token}`
             }
